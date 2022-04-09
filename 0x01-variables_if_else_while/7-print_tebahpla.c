b32c8c064e86579824baaaae67a4a6781f02cb57
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-04-09 09:34:53 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include
+
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet backward
+ * Return: 0
+ * /
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; ==c)
+putchar(c);
+
+putchar('\n');
+
+return (0)
+}
