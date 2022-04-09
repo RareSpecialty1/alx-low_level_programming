7f00a3d7d393c23700d0d538f2a8a2d3cd32ae18
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-04-09 12:20:23 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %d is %s\n", n % 10;
+printf("Last digit of %d is %s\n", n, n % 10,
 ((n % 10) == 0) ? "and is 0"
 : (((n % 10) > 5) ? "and is greater than 5"
-: "and is less than 6 and not 0));
+: "and is less than 6 and not 0"));
 
 return (0);
 }
