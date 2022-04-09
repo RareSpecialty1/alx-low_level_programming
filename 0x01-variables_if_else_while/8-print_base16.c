6ca763ae159dc0ee721a5f5ad8f47512f3a8433b
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-04-09 10:10:59 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,6 +2,7 @@
 
 /**
  * main - entry point
+ *
  * Description: Print all the digits of base 16 in lowercase
  * Return: 0
  */
@@ -11,7 +12,9 @@ char x;
 
 for (x = '0'; x <= '9'; ++x)
 putchar(x);
-for (x = ('a'; x <= 'f'; ++x)
+for (x = 'a'; x <= 'f'; ++x)
+putchar(x);
+
 putchar('\n');
 
 return (0);
