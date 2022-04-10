a3906965264088e45be287a677fc3d434922cbc4
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-04-10 03:35:36 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
-return (0)
+return (0);
 }
