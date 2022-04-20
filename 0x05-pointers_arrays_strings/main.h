4524fad9c88f257516de2ea9374f2e8b39b69ab7
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-04-20 13:56:15 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-main.h
+#ifndef _MAIN_H
+#define _MAIN_H
 
+void _putchar (char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,5 +12,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
+#endif
