ac86e08aecb9bf1bf0cc468300175a85198c4e91
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-05-25 04:51:00 -0700

    A function that prints all elements of list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,4 +24,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endi
+#endif
