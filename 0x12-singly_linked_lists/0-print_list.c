dbe22fb684b08be7eed2bf91f2fcfcf3c755f181
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-05-29 03:20:33 -0700

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,29 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list - is a function that prints all elements of a list_t list
  * @h: is the string input
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nodes = 0;
 
-	nelem = 0;
-
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-
+		{
 			printf("[%d] (%s)\n", 0, "nil");
-
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
-		nelem++;
+		nodes++;
 	}
-	return (nelem);
+	return (nodes);
 }
-
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,20 +16,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* #ifndef LISTS_H */
