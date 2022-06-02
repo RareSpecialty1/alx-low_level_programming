173c8f74bc5c7a27c69237ceb5d849ce0f7ed02e
RareSpecialty1 <abdulyekeenololade@gmail.com>
2022-06-02 08:35:46 -0700

    A function that creates a file

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -1,32 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 on success, -1 on failure
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int create_file(const char *filename, char *text_content)
+int main(int ac, char **av)
 {
-int fd, rstatus, i;
+    int res;
 
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
-		return (-1);
-}
-
-close(fd);
-return (1);
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
